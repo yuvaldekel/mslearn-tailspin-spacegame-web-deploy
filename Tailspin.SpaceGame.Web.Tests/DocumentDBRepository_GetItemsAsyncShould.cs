9857c27d44ca5aa48c35853723c86d213cc01cdc
Yuval Dekel <yuvddd05@gmail.com>
2024-02-19 17:47:37 +0200

    all kind of tests

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq.Expressions;
+using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using TailSpin.SpaceGame.Web;
